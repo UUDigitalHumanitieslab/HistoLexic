65affbaca41401ae685f081e03a0f0b5628cd9b6
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2014-11-20 10:40:22 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-HistoLexic
-==========
+# HistoLexic
 
-Demonstrator for relaying to INL LexiconService for lemmatization
+HistoLexic is a demonstrator for relaying to [INL LexiconService](http://www.succeed-project.eu/wiki/index.php/Lexicon_Service) for lemmatization. The repository currently consists of the following: 
+
+- `histolexic.py`: A standalone Python script for demonstrating the various uses of the LexiconService webservice. Uses the [requests](http://docs.python-requests.org/en/latest/) package. 
+- `histolexic.html`: An example implementation of lemmatization in JavaScript. Uses [jQuery](http://jquery.com/) and the plugin [qTip](http://qtip2.com/). 
+
+## Demo
+
+There's an online demo of the lemmatization available at http://uudigitalhumanitieslab.github.io/HistoLexic/
---
+++ histolexic.py
@@ -0,0 +1,71 @@
+# This is a basic standalone version of the HistoLexic demontstrator in Python
+# Uses the requests package (http://docs.python-requests.org/en/latest/)
+
+import requests
+
+BASE_URL = 'http://sk.taalbanknederlands.inl.nl/LexiconService/lexicon/'
+LEMMA_URL = 'get_lemma'
+WORDFORM_URL = 'get_wordforms'
+EXPAND_URL = 'expand'
+
+def find_lemma(wordform):
+    print '====='
+    print 'Finding lemmata of ' + wordform
+
+    search_term = dict()
+    search_term['database'] = 'lexicon_service_db'
+    search_term['wordform'] = wordform
+    #search_term['year_from'] = 1600
+    #search_term['year_to'] = 2000
+    #search_term['pos'] = 'VRB'
+
+    r = requests.get(BASE_URL + LEMMA_URL, params=search_term, headers={'Accept': 'application/json'})
+    print 'url: ' + r.url
+    print 'lemmata: '
+
+    for j in r.json()['lemmata_list']:
+        for l in j['found_lemmata']:
+            print l['lemma'] + ' - ' + l['pos'] + ', ', 
+
+def find_wordform(lemma):
+    print ''
+    print '====='
+    print 'Finding wordforms of ' + lemma
+
+    search_term = dict()
+    search_term['database'] = 'lexicon_service_db'
+    search_term['lemma'] = lemma
+    #search_term['year_from'] = 1900
+    #search_term['year_to'] = 2000
+    #search_term['pos'] = 'VRB'
+
+    r = requests.get(BASE_URL + WORDFORM_URL, params=search_term, headers={'Accept': 'application/json'})
+    print 'url: ' + r.url
+    print 'wordforms:'
+
+    for j in r.json()['wordforms_list']:
+        print ', '.join(j['found_wordforms'])
+
+def expand(wordform):
+    print '====='
+    print 'Expanding wordform ' + wordform
+
+    search_term = dict()
+    search_term['database'] = 'lexicon_service_db'
+    search_term['wordform'] = wordform
+    #search_term['year_from'] = 1600
+    #search_term['year_to'] = 2000
+    #search_term['pos'] = 'VRB'
+
+    r = requests.get(BASE_URL + EXPAND_URL, params=search_term, headers={'Accept': 'application/json'})
+    print 'url: ' + r.url
+    print r.request.headers
+    print 'wordforms:'
+
+    for j in r.json()['wordforms_list']:
+        print ', '.join(j['found_wordforms'])
+
+if __name__ == "__main__":
+    find_lemma('bank')
+    find_wordform('bank')
+    expand('bank')
